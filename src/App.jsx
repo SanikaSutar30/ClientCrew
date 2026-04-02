b53cfc37a257217d0095434ed5e9bb971d76c46a
SanikaSutar30 <sanikasutar30@gmail.com>
2026-04-02 11:20:54 +0530

    chore(app): connect app entry with route configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import AppRoutes from "./routes/AppRoutes";
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-green-600">ClientCrew 🚀</h1>
-    </div>
+    <>
+      <AppRoutes />
+    </>
   );
 }
 
