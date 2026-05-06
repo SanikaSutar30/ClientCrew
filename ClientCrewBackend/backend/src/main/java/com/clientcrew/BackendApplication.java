f2897546359a0a3dc94d8b6143d7b73b125c9a08
SanikaSutar30 <sanikasutar30@gmail.com>
2026-05-06 19:22:51 +0530

    Add Spring Boot backend starter class

---
+++ ClientCrewBackend/backend/src/main/java/com/clientcrew/BackendApplication.java
@@ -0,0 +1,17 @@
+package com.clientcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
+
+
+//cd E:\JavaFullStackProject\ClientCrew\ClientCrewBackend\backend
+//mvn spring-boot:run
